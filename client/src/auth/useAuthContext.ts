6e83bed5e1747642b3630004af0667c984c2b4f1
kingdaro <kingdaro@gmail.com>
2019-11-09 18:52:56 -0600

    use startTransition

--- client/src/auth/useAuthContext.ts
+++ client/src/auth/useAuthContext.ts
@@ -1,17 +1,23 @@
-import { useMemo, useState } from "react"
+import { useMemo, useState, useTransition } from "react"
 import createContextWrapper from "../common/createContextWrapper"
 import AuthResource from "./AuthResource"
 
 function useAuth({ resource: initialResource }: { resource: AuthResource }) {
   const [resource, setResource] = useState(initialResource)
+  const [startTransition] = useTransition()
+
   return useMemo(
     () => ({
       readUser: () => resource.readUser(),
       readToken: () => resource.readToken(),
-      logIn: () => setResource(new AuthResource({ logIn: true })),
+      logIn: () => {
+        startTransition(() => {
+          setResource(new AuthResource({ logIn: true }))
+        })
+      },
       logOut: () => resource.logOut(),
     }),
-    [resource],
+    [resource, startTransition],
   )
 }
 
